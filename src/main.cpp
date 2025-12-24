c2cb6ee1a4b24c81dc55801b529d3060522af372
STAKXX002 <matrixmaximus007@gmail.com>
2025-12-24 13:18:03 +0530

    docs: add PathInput to README and update usage

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,28 @@
 #include "FileWatcher.h"
 #include "FileClassifier.h"
 #include "FileMover.h"
+#include "PathInput.h"
 
-int main(){
-    std::string sourceDir = "C:\\Users\\KIIT\\Downloads";
-    std::string targetRoot = "C:\\Dwn";
 
-    std::vector<std::filesystem::path>files = FileWatcher::getFiles(sourceDir);
+int main() {
+    std::filesystem::path sourceDir = PathInput::getSourcePath();
+    std::filesystem::path targetRoot = PathInput::getTargetPath();
 
-    for(const auto& file : files){
-        if(std::filesystem::is_directory(file)) continue;
+    std::vector<std::filesystem::path> files =
+        FileWatcher::getFiles(sourceDir.string());
+
+    for (const auto& file : files) {
+        if (std::filesystem::is_directory(file)) continue;
 
         std::string fileType = FileClassifier::getFileType(file);
         std::string datePath = FileClassifier::getDateSubfolder(file);
-        std::filesystem::path targetDir = targetRoot + "\\" + fileType + "\\" + datePath;
+
+        std::filesystem::path targetDir =
+            targetRoot / fileType / datePath;
 
         FileMover::moveFile(file, targetDir);
     }
 
-    std::cout << "AutoRelay finished moving files." << std::endl;
-
+    std::cout << "AutoRelay finished moving files.\n";
     return 0;
 }
